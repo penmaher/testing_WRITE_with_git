7cbbd5001bb90cc578d2dcc3f3171829b6cd8161
Penelope Maher <p.maher@exeter.ac.uk>
2017-03-21 10:37:33 +0000

    Initial commit with automated bash scripts where possible.

---
+++ user_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git_path="/home/$username_exe/Documents/PublicCode/TestingTrackChanges"
+paper_path=${git_path}
+editing_path=${git_path}/Editing
+export git_path
+tex_name=article_
+declare -a author_list=('penny' 'steve')
+branch_prefix='branch_'
+
+
