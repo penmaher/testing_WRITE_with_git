53063f1d705776b88fb4f5b2b133f55ef1fed6be
Penelope Maher <p.maher@exeter.ac.uk>
2017-03-21 11:09:14 +0000

    Incorrect user config for branch and add elements to gitignore

--- user_config.sh
+++ user_config.sh
@@ -4,8 +4,8 @@ git_path="/home/$username_exe/Documents/PublicCode/TestingTrackChanges"
 paper_path=${git_path}
 editing_path=${git_path}/Editing
 export git_path
-tex_name=article_
+tex_name=article
 declare -a author_list=('penny' 'steve')
-branch_prefix='branch_'
+branch_prefix='test_'
 
 
